02f5f555831cea93502fd98cd1804f73901382ea
kensukefurusato <kensuke@quantum-box.com>
2021-02-20 15:05:53 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import reactDom from 'react-dom';
 
 class App extends Component {
   render() {
-    return <h1>hello, world!</h1>;
+    return(
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("I am clicked!")}} />
+      </React.Fragment>
+    )
   }
 }
 
